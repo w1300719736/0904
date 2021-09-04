a536932ddcd9e6a9f1ef801813c18c59ea0b3d49
wuhao <1300719736@qq.com>
2021-09-04 23:31:32 +0800

    add 0904 修改备注 主分支的

--- UserDao.java
+++ UserDao.java
@@ -1,6 +1,10 @@
+/**
+ * @Description: dd
+ * @Author WuHao
+ * @Date: 2021/9/4 23:31
+ * @Version 1.0
+ */
 package ujiuye.dao;
-
-
 
 
 import org.apache.commons.dbutils.QueryRunner;
@@ -12,12 +16,13 @@ import java.sql.SQLException;
 
 public class UserDao {
     QueryRunner qr = JdbcUtil.getQueryRunner();
+
     public User login(String username, String pwd){
         User user = null;
         String sql = "select * from tb_user where username = ? and pwd = ?";
         System.out.println(username + " "+pwd);
         try {
-           user = qr.query(sql,new BeanHandler<>(User.class),username,pwd);
+            user = qr.query(sql,new BeanHandler<>(User.class),username,pwd);
         } catch (SQLException e) {
             e.printStackTrace();
         }
