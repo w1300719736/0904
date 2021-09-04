74a159300b3696cd7cf5bcdf14b98807b7aa0e00
wuhao <1300719736@qq.com>
2021-09-04 22:49:04 +0800

    add 0904

---
+++ UserDao.java
@@ -0,0 +1,27 @@
+package ujiuye.dao;
+
+
+
+
+import org.apache.commons.dbutils.QueryRunner;
+import org.apache.commons.dbutils.handlers.BeanHandler;
+import ujiuye.entity.User;
+import ujiuye.util.JdbcUtil;
+
+import java.sql.SQLException;
+
+public class UserDao {
+    QueryRunner qr = JdbcUtil.getQueryRunner();
+    public User login(String username, String pwd){
+        User user = null;
+        String sql = "select * from tb_user where username = ? and pwd = ?";
+        System.out.println(username + " "+pwd);
+        try {
+           user = qr.query(sql,new BeanHandler<>(User.class),username,pwd);
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+        return user;
+    }
+}
+
